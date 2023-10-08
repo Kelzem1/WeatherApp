cb540fd1fe516b28c80320c013e0f945f79a133c
Rodolfo Rolon <kelzemrod@gmail.com>
2023-10-08 18:24:33 +0200

    Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import Navbar from './Components/Navbar'
+import Cards from './Components/Styles/Cards'
 
 function App() {
 
   return (
-   <div className='font-bold text-2xl'>
-    <h1>Llueve o no</h1>
-    <span>"Breve descripcion de la app"</span>
+   <div className='font-bold'>
+    <Cards title="Hola" description="Hace calor"/>
     <Navbar />
    </div>
   )
---
+++ src/Components/Styles/Cards.jsx
@@ -0,0 +1,10 @@
+const Cards =({title, description, image})=>{
+    return(
+        <div className="">
+            <h1>{title}</h1>
+            <h4>{description}</h4>
+            <span>{image}</span>
+        </div>
+    )
+}
+export default Cards
