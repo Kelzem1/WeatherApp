6468fe785a9354278c61d3f716b2d766799c7170
Rodolfo Rolon <kelzemrod@gmail.com>
2023-10-08 21:50:02 +0200

    Cards styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,9 @@ import Cards from './Components/Styles/Cards'
 function App() {
 
   return (
-   <div className='font-bold'>
+   <div className='font-bold m-5 m-auto flex-col w-full h-full'>
     <Cards title="Hola" description="Hace calor"/>
+    <Cards title="Esta nublao" description="Hace frio"/>
     <Navbar />
    </div>
   )
