cd41bbf305a84a020e7c054c97a41d546d8cf7a6
Rodolfo Rolon <kelzemrod@gmail.com>
2023-10-12 16:01:03 +0200

    Api more

--- src/Components/Services/GetApi.jsx
+++ src/Components/Services/GetApi.jsx
@@ -1,22 +1,73 @@
+import { useState } from "react"
+import CurrentData from "../CurrentData"
+
 const ApiKey = '147ed690441ebcf171c54ca58cdd922d'
 
-
-let lat = ''
-let lon = ''
 
 
 
 
 const GetApi = () =>{
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${ApiKey}`
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?&appid=${ApiKey}&lang=es`
+  let city = '&q='
+  let ApiForecast = `https://api.openweathermap.org/data/2.5/forecast?&appid=${ApiKey}&lang=es`
 
-  return fetch(apiUrl)
-  .then(res => res.json()
-  .then(response => {
-    const {data} = response
-    console.log(data)
-  })
+  const [weather, setWeather]= useState([])
+  const [forecast, setForecast] = useState([])
+  const [loading, setLoading] = useState([false])
+  const [show, setShow] =useState(false)
+  const [location, setLocation] = useState([""])
+
+  const getLocation = async(loc) =>{
+    setLoading(true)
+    setLocation(loc)
+
+
+    //weather
+
+    apiUrl = apiUrl + city + loc;
+
+    await fetch(apiUrl)
+    .then((response) => {
+      if(!response.ok) throw {response}
+      return response.json()
+    }).then((weatherData) =>{
+      console.log(weatherData)
+      setWeather(weatherData)
+    }).catch(error =>{
+      console.log(error)
+      setLoading(false)
+      setShow(false)
+    })
+
+    //Forecast
+
+    ApiForecast = ApiForecast + city + loc;
+
+    await fetch(ApiForecast)
+    .then((response) => {
+      if(!response.ok) throw {response}
+      return response.json()
+    }).then((forecastData) =>{
+      console.log(forecastData)
+      setForecast(forecastData)
+
+      setLoading(false)
+      setShow(true)
+
+    }).catch(error =>{
+      console.log(error)
+      setLoading(false)
+      setShow(false)
+    })
+  }
+
+  return(
+    <div>
+      <CurrentData grados={weatherData} />
+    </div>
   )
+
 }
 
 export default GetApi
