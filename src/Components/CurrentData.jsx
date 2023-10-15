ed7b3e4b7407677f765bcbb5853f85dfc1cb3622
Rodolfo Rolon <kelzemrod@gmail.com>
2023-10-15 18:03:52 +0200

    Name, temp fixed

--- src/Components/CurrentData.jsx
+++ src/Components/CurrentData.jsx
@@ -1,13 +1,27 @@
 import GetApi from "./Services/GetApi"
+import Spinner from "./Spinner"
 
 const CurrentData = ({loadingData, showData, weather})=>{
+    if (loadingData){
+        return <Spinner />
+    }
     return(
-        <div className="bg-sky-400 mt-2 p-5 text-stone-200 w-2/12 m-auto flex-col justify-center self-center rounded-sm">
-            <h1>{weather.name}</h1>
-            <h1>{(weather.main.temp -273.15).toFixed(1)}C</h1>
-            <h2>{}</h2>
+        <div>
+               {
+                showData === true ? (
+            <div className="bg-sky-400 mt-2 p-5 text-stone-200 w-2/12 m-auto flex-col justify-center self-center rounded-sm">
+                <h1>{weather.name}</h1>
+                <h1>{(weather.main.temp -273.15).toFixed(1)}C</h1>
+                <h2>{}</h2>
+            </div>
+            ):(
+                <h2>Sin datos</h2>
+            )
+        }
         </div>
-    )
+     
+       
+    )  
 }
 
 export default CurrentData
