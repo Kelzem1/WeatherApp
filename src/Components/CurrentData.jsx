26e288431996508b1de77222058e9a68fbbcf255
Rodolfo Rolon <kelzemrod@gmail.com>
2023-11-04 08:38:03 +0100

    Forecast in current data 3. All fixed

--- src/Components/CurrentData.jsx
+++ src/Components/CurrentData.jsx
@@ -37,15 +37,19 @@ const CurrentData = ({loadingData, showData, weather, forecast})=>{
         url = "http://openweathermap.org/img/w/"
         iconUrl = url + weather.weather[0].icon + ".png"
 
-        iconUrl3 = url + forecast.list[1].weather[0].icon + "png"
-        iconUrl6 = url + forecast.list[2].weather[0].icon + "png"
-        iconUrl9 = url + forecast.list[3].weather[0].icon + "png"
-        iconUrl12 = url + forecast.list[4].weather[0].icon + "png"
+        iconUrl3 = url + forecast.list[1].weather[0].icon + ".png"
+        iconUrl6 = url + forecast.list[2].weather[0].icon + ".png"
+        iconUrl9 = url + forecast.list[3].weather[0].icon + ".png"
+        iconUrl12 = url + forecast.list[4].weather[0].icon + ".png"
 
         forecastDate3 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' + forecast.list[1].dt_txt.substring(11, 13)
-        forecastDate6 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' + forecast.list[1].dt_txt.substring(11, 13)
-        forecastDate9 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' + forecast.list[1].dt_txt.substring(11, 13)
-        forecastDate12 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' + forecast.list[1].dt_txt.substring(0, 4) + ' ' + forecast.list[1].dt_txt.substring(11, 13)
+
+        forecastDate6 = forecast.list[2].dt_txt.substring(8, 10) + '/' + forecast.list[2].dt_txt.substring(5, 7) + '/' + forecast.list[2].dt_txt.substring(0, 4) + ' ' + forecast.list[2].dt_txt.substring(11, 13)
+
+        forecastDate9 = forecast.list[3].dt_txt.substring(8, 10) + '/' + forecast.list[3].dt_txt.substring(5, 7) + '/' + forecast.list[3].dt_txt.substring(0, 4) + ' ' + forecast.list[3].dt_txt.substring(11, 13)
+
+        forecastDate12 = forecast.list[4].dt_txt.substring(8, 10) + '/' + forecast.list[4].dt_txt.substring(5, 7) + '/' + forecast.list[4].dt_txt.substring(0, 4) + ' ' + forecast.list[4].dt_txt.substring(11, 13)
+
 
 
     }
@@ -62,20 +66,19 @@ const CurrentData = ({loadingData, showData, weather, forecast})=>{
                     <div className="flex-col w-6/12 justify-center align-middle m-auto p-3">
                         <h1 className="text-center text-2xl font-semibold">{weather.name}</h1>
                         <h1 className="text-center">{date}</h1> 
-                        
                     </div>
-                    
-                    <div className="flex-col justify-center w-6/12 m-auto p-3">
-                        <h1 className="text-center font-semibold pb-3 text-xl">Temperatura: </h1>
-                        <p className="text-center">{(weather.main.temp_max -273.15).toFixed(1) + "°C"}</p>
+
+                    <div className="flex-col justify-center text-center w-6/12 m-auto p-3">
+                        <p className="flex justify-center"><img src={iconUrl} alt="icon" /></p>
+                        <h2 className="capitalize">{weather.weather[0].description}</h2> 
+                    </div>
+
+                    <div className="flex justify-center w-6/12 m-auto p-3">
+                        <p className="flex align-middle">{(weather.main.temp_max -273.15).toFixed(1) + "°C"}</p>
                         <h1 className="text-center flex text-5xl justify-center font-bold p-5">{(weather.main.temp -273.15).toFixed(1) + "°C"}</h1>
                         <p className="text-center">{(weather.main.temp_min -273.15).toFixed(1) + "°C"}</p>
                     </div>
-                    <div className="flex-col justify-center text-center w-6/12 m-auto p-3">
-                        <p className="flex justify-center"><img src={iconUrl} alt="icon" /></p>
-                        <h2 className="capitalize">{weather.weather[0].description}</h2>
-                        
-                </div>
+                    
                 
             </div>
 
@@ -97,23 +100,47 @@ const CurrentData = ({loadingData, showData, weather, forecast})=>{
                 <hr />
                 
                 <div className="forecast-4 flex">
-                    <div className="fore3h">
-                    <p>{forecastDate3}</p>
-                    <p className="flex-col"><img src={iconUrl3} alt="icon" /></p>
+
+                    <div className="fore3h flex-col">
+                    <p>{forecastDate3}h</p>
+                    <p>{(forecast.list[1].main.temp -273.15).toFixed(1) +"°C"} </p>
+
+                    <p className="">
+                    <img src={iconUrl3} alt="icon" />
+                    {forecast.list[1].weather[0].description}
+                    </p>
+
                     </div>
 
-                    <div className="fore6h">
-                    <p>{forecastDate6}</p>
-                    <p className="flex"><img src={iconUrl6} alt="icon" /></p>
+                    <div className="fore6h flex-col">
+                    <p>{forecastDate6}h</p>
+                    <p>{(forecast.list[2].main.temp -273.15).toFixed(1) +"°C"} </p>
+
+                    <p className="">
+                    <img src={iconUrl6} alt="icon" />
+                    {forecast.list[2].weather[0].description}
+                    </p>
+
                     </div>
 
-                    <div className="fore9h">
-                    <p>{forecastDate9}</p>
-                    <p className="flex"><img src={iconUrl9} alt="icon" /></p>
+                    <div className="fore9h flex-col">
+                    <p>{forecastDate9}h</p>
+                    <p>{(forecast.list[3].main.temp -273.15).toFixed(1) +"°C"} </p>
+
+                    <p className=""><img src={iconUrl9} alt="icon" />
+                    {forecast.list[3].weather[0].description}
+                    </p>
+
                     </div>
-                    <p className="flex"><img src={iconUrl12} alt="icon" /></p>
+                    
                     <div className="fore12h">
-                        
+                    <p>{forecastDate12}h</p>
+                    <p>{(forecast.list[4].main.temp -273.15).toFixed(1) +"°C"} </p>
+
+                    <p className=""><img src={iconUrl12} alt="icon" />
+                    {forecast.list[4].weather[0].description}
+                    </p>
+
                     </div>
                 </div>
 
@@ -126,7 +153,7 @@ const CurrentData = ({loadingData, showData, weather, forecast})=>{
                 
             
             ):(
-                <Forecast />
+                <div></div>
             )
         }
                 
